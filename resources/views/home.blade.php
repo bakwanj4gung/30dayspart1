0728dcc669bcc0434e9335b212aa0c2621307afb
bakwanj4gung <alimuhammadfaqih@gmail.com>
2024-08-30 01:01:56 +0700

    commit awal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href={{ asset('assets/css/app.css') }}>
+        <title>Tata Produk</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=SUSE:wght@100..800&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <nav class="navigation-bar">
+                <div class="logo">
+                    <img src={{ asset('assets/img/logo.png') }} alt="Logo">
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Kategori</a>
+                    </li>
+                    <li>
+                        <a href="#">Semua Produk</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            
+        </main>
+    </body>
+</html>
